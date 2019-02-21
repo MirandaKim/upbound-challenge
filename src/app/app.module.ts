93a8188904737df343ac7c044cbf397bebec46df
Miranda Hoover <mkhoover89@gmail.com>
2019-02-21 13:30:06 -0800

    Added basic components with minor bootstrap styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
+import { CardMenuComponent } from './components/card-menu/card-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    CardListComponent,
+    CardComponent,
+    CardMenuComponent,
   ],
   imports: [
     BrowserModule,
