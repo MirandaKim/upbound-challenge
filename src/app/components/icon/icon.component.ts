bdfd1a708a1600b6f34ddb03a711e9971c941e33
Miranda Hoover <mkhoover89@gmail.com>
2019-03-03 17:39:25 -0800

    Icon component now allows for customization with fill color, hover fill color, and sizes.

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -8,11 +8,21 @@ import { Component, OnInit, Input } from '@angular/core';
 export class IconComponent implements OnInit {
 
   @Input()
-  iconType: string;
+  iconType: string; // e.g. icon-trash, icon-database, icon-eye (see src/assets/symbol-defs.svg all svg ids)
+
+  @Input()
+  iconSize: string = "medium"; // e.g. small, medium, large (see ./icon.component.scss for all size options)
+
+  @Input()
+  iconColor: string = "medium-grey"; // e.g. black, white, orange (see ./icon.component.scss for all size options)
+
+  @Input()
+  iconColorOnHover: string = "none"; // e.g. black, white, orange (see ./icon.component.scss for all size options)
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
