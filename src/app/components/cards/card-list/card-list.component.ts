867d3c5dcbc9f0bac7b28a6b3b0d3531cd21bb6f
Miranda Hoover <mkhoover89@gmail.com>
2019-03-06 18:06:44 -0800

    Updated card-list for new filter system. Cleaned up and added additional documentation/organization.

--- src/app/components/cards/card-list/card-list.component.ts
+++ src/app/components/cards/card-list/card-list.component.ts
@@ -3,6 +3,35 @@ import { CardsService } from 'src/app/services/cards.service';
 import { Card } from 'src/app/interfaces/card.interface';
 import { FiltersService } from 'src/app/services/filters.service';
 import { Filter } from 'src/app/interfaces/filter.interface';
+import { FilterListService } from 'src/app/services/filter-list.service';
+
+/**********************************************************/
+/*                                                       */
+/*   Filter List Service                                */
+/*                                                     */
+/******************************************************/
+/*
+
+Display a list of cards from the cards api.
+This list can be filtered at the component level.
+
+*****************
+*   Contents:   *
+*****************
+
+  # Properties
+  # Constructor
+  # On Init
+  # Protected
+    > Configs
+    > Get Cards
+  # Private
+    > Apply Filter Change
+  # On Destroy
+  # For Testing
+
+******************/
+
 
 @Component({
   selector: 'app-card-list',
@@ -15,23 +44,22 @@ export class CardListComponent implements OnInit {
   /*   # Properties                          */
   /******************************************/
 
-  protected cardList_full: any[] = [];
-  protected cardList_filtered: any[] = [];
+  protected cardList_full: any[] = []; // complete list of cards
+  protected cardList_filtered: any[] = []; // filtered list of cards (filtered for display)
 
-  protected allowedFilters = [
-    'campaignId'
+  protected allowedFilters = [ // list of card properties, which may be used to fitler the cards
+    'campaignId',
+    'cardTitle',
+    'id',
+    'currentWorkflow'
   ];
-  protected filters = {
-    campaignId: {
-      value: '',
-      filterKey: 'campaignId'
-    }
-  };
+  protected filterWatch; // watching for fitler updates from FiltersService
 
-  protected resReceived = false;
-  private cardsService: CardsService;
+  protected apiResponseReceived = false; // Received response from API (note: 404 still counts as a response).
 
-  private filtersService: FiltersService;
+  private cardsService: CardsService; // Service for accessing the cards api
+  private filtersService: FiltersService; // Service used for tracking filters
+  private filterListService: FilterListService; // Service for filtering list of cards
 
 
   /********************************************/
@@ -40,10 +68,12 @@ export class CardListComponent implements OnInit {
 
   constructor(
     cardsService: CardsService,
-    filtersService: FiltersService
+    filtersService: FiltersService,
+    filterListService: FilterListService
   ) {
     this.cardsService = cardsService;
     this.filtersService = filtersService;
+    this.filterListService = filterListService;
   }
 
   /********************************************/
@@ -51,22 +81,104 @@ export class CardListComponent implements OnInit {
   /******************************************/
 
   ngOnInit() {
-    // this.getCards();
-    this.cardList_full = this.testing();
-    this.cardList_filtered = this.cardList_full;
-
-    this.filtersService.onFilterChange.subscribe((res) => {
+    /*
+    Get Card Data
+    */
+    // this.getCards(); // DON'T DELETE! this is the actual api call, apply this when not testing
+    this.cardList_full = this.getTestData(); // not the actual data--use instead of api for testing
+    this.cardList_filtered = this.cardList_full; // for testing only
+    /*
+    Configure Filter Info
+    */
+    this.configureListFilter(); // set configs for filtering the cards with the FilterListService
+    /*
+    Watch for filter updates
+    */
+    this.filterWatch = this.filtersService.onFilterChange.subscribe((res) => {
       this.applyFilterChange(res);
-      console.log('Hey! Filter Changed');
-      console.log(res);
     });
   }
 
+  /********************************************/
+  /*   # Protected                           */
+  /******************************************/
+
+  /***************
+  *  > Configs   *
+  ***************/
+  /*
+  Configure List Filter:
+  Provide the FilterListService with a list of properties
+  that can be used to filter the list of cards.
+  */
+  protected configureListFilter(){
+    this.filterListService.setConfig(this.allowedFilters);
+  }
+
+  /*****************
+  *  > Get Cards   *
+  *****************/
+  /*
+  Get Cards:
+  Access the API for a full list of cards
+  */
+  protected getCards(): void{
+    /*Subscribe to Cards Service*/
+    this.cardsService.readAll().subscribe((res) => {
+      /*
+      Valid: check if the response has the property 'items',
+      which should contain the cards. If the response is not valid,
+      it is likely due to a 404.
+      */
+      let valid = res.hasOwnProperty('items');
+      if(valid){
+        this.cardList_full = res['items'];// get list of cards from response
+        this.cardList_filtered = this.cardList_full;
+      }
+      this.apiResponseReceived = true; // let the component know the get request is complete
+    }, (error) => {
+      console.log(error);
+      this.apiResponseReceived = true; // let the component know the get request is complete
+    });
+
+  }
+
+  /********************************************/
+  /*   # Private                             */
+  /******************************************/
+
+  /***************************
+  *  > Apply Filter Change   *
+  ***************************/
+
+  /*
+  Apply Filter Change:
+  When a filter value has changed, send the new filter to the FilterListService.
+  If the filter can be applied, then get a list of cards filtered by the new value.
+  */
+  private applyFilterChange(changedFilter: Filter){
+    let validFilter = this.filterListService.applyFilter(changedFilter);
+    if(validFilter){
+      this.cardList_filtered = this.filterListService.filterList(this.cardList_full);
+    }
+  }
+
+  /********************************************/
+  /*   # On Destroy                          */
+  /******************************************/
+
+  ngOnDestroy(){
+    this.filterWatch.unsubscribe();
+  }
+
+  /********************************************/
+  /*   # For Testing                         */
+  /******************************************/
   /*
   TEST FUNCTION
   Dev note: this function is only for development purposes when API cannot be accessed.
   */
-  testing() {
+  private getTestData() {
     let testCards = [
       {
         "campaignId": "CN201701182",
@@ -143,80 +255,87 @@ export class CardListComponent implements OnInit {
         "userId": "5d6117b9ae114c83bb53cfdd8c722e78",
         "createdAt": "2017-02-06T11:14:45.131Z",
         "updatedAt": "2017-02-06T11:16:44.344Z"
+      },
+      {
+        "campaignId": "CN201701182!!",
+        "cardTitle": "Title 1",
+        "cardDescription": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ",
+        "primaryMediaUrl": "https://loremflickr.com/750/400",
+        "cardStartDate": "0001-01-01T00:00:00Z",
+        "cardEndDate": "0001-01-01T00:00:00Z",
+        "availableQuantity": 32,
+        "likes": 10,
+        "shares": 20,
+        "views": 30,
+        "subscribers": 0,
+        "unSubscribers": 0,
+        "open": 0,
+        "discard": 0,
+        "totalRevenue": 0,
+        "listOfPlans": [
+          {
+            "price": { "amount": "47800", "currency": "JPY", "currencySymbol": "¥" }
+          }
+        ],
+        "locations": [
+          {
+            "latitude": "",
+            "longitude": "",
+            "area": "",
+            "city": "",
+            "country": "",
+            "placeId": "ChIJx8SRZhAEGTkRwsZ7Hq_dJPs",
+            "description": "Seattle, Washington, USA"
+          }
+        ],
+        "currentWorkflow": "paused",
+        "campaignEndDate": "2017-02-28T18:59:59.999Z",
+        "id": "c3d444c3d2774263ab93598fb0705374",
+        "userId": "5d6117b9ae114c83bb53cfdd8c722e78",
+        "createdAt": "2017-02-06T11:14:45.131Z",
+        "updatedAt": "2017-02-06T11:16:44.344Z"
+      },
+      {
+        "campaignId": "CN201701!",
+        "cardTitle": "Title 1",
+        "cardDescription": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ",
+        "primaryMediaUrl": "https://loremflickr.com/750/400",
+        "cardStartDate": "0001-01-01T00:00:00Z",
+        "cardEndDate": "0001-01-01T00:00:00Z",
+        "availableQuantity": 32,
+        "likes": 10,
+        "shares": 20,
+        "views": 30,
+        "subscribers": 0,
+        "unSubscribers": 0,
+        "open": 0,
+        "discard": 0,
+        "totalRevenue": 0,
+        "listOfPlans": [
+          {
+            "price": { "amount": "47800", "currency": "JPY", "currencySymbol": "¥" }
+          }
+        ],
+        "locations": [
+          {
+            "latitude": "",
+            "longitude": "",
+            "area": "",
+            "city": "",
+            "country": "",
+            "placeId": "ChIJx8SRZhAEGTkRwsZ7Hq_dJPs",
+            "description": "Seattle, Washington, USA"
+          }
+        ],
+        "currentWorkflow": "paused",
+        "campaignEndDate": "2017-02-28T18:59:59.999Z",
+        "id": "c3d444c3d2774263ab93598fb0705374",
+        "userId": "5d6117b9ae114c83bb53cfdd8c722e78",
+        "createdAt": "2017-02-06T11:14:45.131Z",
+        "updatedAt": "2017-02-06T11:16:44.344Z"
       }
     ];
     return testCards;
-  }
-
-  /********************************************/
-  /*   # Protected                           */
-  /******************************************/
-
-  /*****************
-  *  > Get Cards   *
-  *****************/
-  /*
-  Get Cards:
-  Access the API for a full list of cards
-  */
-
-  protected getCards(): void{
-    /*Subscribe to Cards Service*/
-    this.cardsService.readAll().subscribe((res) => {
-      /*
-      Valid: check if the response has the property 'items',
-      which should contain the cards. If the response is not valid,
-      it is likely due to a 404.
-      */
-      console.log(res);
-      let valid = res.hasOwnProperty('items');
-      if(valid){
-        this.cardList_full = res['items'];// get list of cards from response
-        this.cardList_filtered = this.cardList_full;
-      }
-      this.resReceived = true; // let the component know the get request is complete
-    }, (error) => {
-      console.log(error);
-      this.resReceived = true; // let the component know the get request is complete
-    });
-
-  }
-
-  protected applyFilterChange(changedFilter){
-    console.log('Apply Filter Change:')
-    console.log(changedFilter);
-    let valid = this.checkValidFilterChange(changedFilter);
-    if(valid){
-      this.filters[changedFilter.property].value = changedFilter.value;
-      this.filterDisplayList();
-    }else {
-      console.log('Filter not applied. Ignore.');
-    }
-  }
-
-  protected checkValidFilterChange(changedFilter){
-    let validProp = this.allowedFilters.indexOf(changedFilter.property) > -1;
-    return validProp;
-  }
-
-  protected filterDisplayList(){
-    // start with full list of cards
-    let filteredCards = this.cardList_full;
-    /*
-    For each of the allowed filters,
-    filter the list by the filter's value and make this the new filtered list of cards.
-    */
-    for(let i=0; i<this.allowedFilters.length; i++){
-      let filterType = this.allowedFilters[i];
-      let filterKey = this.filters[filterType]['filterKey'];
-      let filterValue = this.filters[filterType].value;
-      if(filterValue.length > 0) {
-        filteredCards = filteredCards.filter((card) => {
-          return card[filterKey] == filterValue;
-        });
-      }
-    }
-    this.cardList_filtered = filteredCards;
   }
 
 }
