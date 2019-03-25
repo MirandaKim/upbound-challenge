8a4904370a41d999191b1a12d8baa9d6681a36b7
Miranda Hoover <mkhoover89@gmail.com>
2019-03-25 16:58:09 -0700

    Styled the date-selector in the header. This component is not functional and still contains filler content.

--- src/app/components/date-selector/date-selector.component.ts
+++ src/app/components/date-selector/date-selector.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DateSelectorComponent implements OnInit {
 
+  protected today: Date;
+  protected selectedDate: number = 0; // 0 is today, 1 is tomorrow, -1 is yesturday, and so on...
+
   constructor() { }
 
   ngOnInit() {
+    this.setCurrentDate();
+  }
+
+  protected setCurrentDate(){
+    this.today = new Date();
   }
 
 }
