ef3d9c33b468c63dc0937219ea3cbc502bbe3b92
Miranda Hoover <mkhoover89@gmail.com>
2019-04-05 15:51:28 -0700

    The resubmit button in the rejected card footer now triggers an update via the card-manager service.

--- src/app/components/cards/card-footer-rejected/card-footer-rejected.component.ts
+++ src/app/components/cards/card-footer-rejected/card-footer-rejected.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Card } from 'src/app/interfaces/card.interface';
+import { CardManagerService } from 'src/app/services/card-manager.service';
 
 @Component({
   selector: 'app-card-footer-rejected',
@@ -9,10 +10,26 @@ import { Card } from 'src/app/interfaces/card.interface';
 export class CardFooterRejectedComponent implements OnInit {
 
   @Input()
-  card: Card;
+  protected card: Card;
+  protected cardIdKey: string = 'id'; // property of a Card object where the card's id is stored
+  protected cardStatusKey: string = 'currentWorkflow'; // property of a Card object where the card's status is stored
+  protected statusPendingValue: string = 'pending'; // Value of a card's status when the card is 'pending'--used for resubmitting card.
 
-  constructor() { }
+  protected cardManagerService: CardManagerService;
+
+  constructor(cardManagerService: CardManagerService) {
+    this.cardManagerService = cardManagerService;
+  }
 
   ngOnInit() {}
+
+  protected resubmitCard(){
+    this.card[this.cardStatusKey] = this.statusPendingValue;
+    this.cardManagerService.updateCards();
+  }
+
+  protected onResubmitClick(){
+    this.resubmitCard();
+  }
 
 }
