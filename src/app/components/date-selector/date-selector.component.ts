2b0b06c0c06c49a5802e835f9991be3d49172593
Miranda Hoover <mkhoover89@gmail.com>
2019-05-01 17:44:34 -0700

    Cleaned up the date-selector and added partial logic to display different values as previous/next buttons are clicked. Functionality is not complete and the actual date does not yet change.

--- src/app/components/date-selector/date-selector.component.ts
+++ src/app/components/date-selector/date-selector.component.ts
@@ -1,5 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+/**********************************************************/
+/*                                                       */
+/*   Date Selector (date-selector.component.ts)         */
+/*                                                     */
+/******************************************************/
+/*
+
+  Loop through dates to create a date filter.
+
+*********************************
+*   This module is INCOMPLETE   *
+*********************************
+
+  - Logic to change the selected date has not been created @TODO: Loop through dates (see function shiftDate)
+  - Logic for filtering cards based on the selected date has not been created @TODO: filter cards by selected date.
+
+*****************
+*   Contents:   *
+*****************
+
+  # Properties
+    >
+  # Constructor
+  # On Init
+  # Protected
+
+******************/
+
 @Component({
   selector: 'app-date-selector',
   templateUrl: './date-selector.component.html',
@@ -7,17 +35,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DateSelectorComponent implements OnInit {
 
+  /********************************************/
+  /*   # Properties                          */
+  /******************************************/
+
   protected today: Date;
-  protected selectedDate: number = 0; // 0 is today, 1 is tomorrow, -1 is yesturday, and so on...
+  protected dayOffset: number = 0; // 0 is today, 1 is tomorrow, -1 is yesturday, and so on...
+  protected dayCountdown: string = '1';
+
+  protected selectedDate: Date;
+
+  protected dayPronouns: string[] = [
+    'Yesterday',
+    'Today',
+    'Tomorrow'
+  ];
+
+  /********************************************/
+  /*   # Constructor                         */
+  /******************************************/
 
   constructor() { }
+
+  /********************************************/
+  /*   # On Init                             */
+  /******************************************/
 
   ngOnInit() {
     this.setCurrentDate();
   }
 
-  protected setCurrentDate(){
+  /********************************************/
+  /*   # Protected                           */
+  /******************************************/
+
+  /************************
+  *  > Set Current Date   *
+  ************************/
+
+  /*
+  Set Current Date
+  Find today's date and use it as the initialy selected date.
+  */
+  protected setCurrentDate(): void{
     this.today = new Date();
+    this.selectedDate = new Date();
+  }
+
+  /******************
+  *  > Shift Date   *
+  ******************/
+
+  /*
+  Shift Date:
+  Change the currently selected date by the provided integer.
+  Ex:
+  shiftValue = 1 will change the currently selected date to the next day.
+  shiftValue = -1 will change the currently selected date to the previous day.
+  shiftValue = 10 will change the currently selected date to 10 days ahead.
+
+  WARNING: This functionality is not complete.
+  */
+  protected shiftDate(shiftValue: number): void{
+    this.dayOffset += shiftValue;
+    this.setDayCountdown();
+    console.log('Sorry, looping through dates is not availible at this time.');
+  }
+
+  /*************************
+  *  > Set Day Countdown   *
+  **************************/
+
+  /*
+  Set Day Countdown:
+
+  Create as string value that represents the day difference between today's date
+  and the currently selected date.
+  */
+  protected setDayCountdown(): void{
+    let daysLeft = this.dayOffset + 1;
+    if(daysLeft > 99){
+      this.dayCountdown = '>99';
+    }else if(daysLeft < 1){
+      this.dayCountdown = '<1';
+    }else{
+      this.dayCountdown = daysLeft.toString();
+    }
   }
 
 }
