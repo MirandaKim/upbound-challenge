f0f01a6ae9e34faec66f14ea7e1bcf61c9de16e7
Miranda Hoover <mkhoover89@gmail.com>
2019-05-11 11:21:13 -0700

    Added documentation headers.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,23 @@ import { Component } from '@angular/core';
 import { DeviceStateService } from 'src/app/services/device-state.service';
 import $ from 'jquery';
 
+/**********************************************************/
+/*                                                       */
+/*   App (app.component.ts)                             */
+/*                                                     */
+/******************************************************/
+/*
+
+*****************
+*   Contents:   *
+*****************
+
+  # Properties
+  # Constructor
+  # Private
+
+******************/
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,16 +26,28 @@ import $ from 'jquery';
 })
 export class AppComponent {
 
+  /********************************************/
+  /*   # Properties                          */
+  /******************************************/
+
   protected hasTouchClass = 'has-touch';
   protected bodySelector = 'body';
+
+  /********************************************/
+  /*   # Constructor                         */
+  /******************************************/
 
   constructor(private deviceStateService: DeviceStateService){
     this.watchForTouchEvent();
   }
 
+  /********************************************/
+  /*   # Private                             */
+  /******************************************/
+
   private watchForTouchEvent(){
     var touchListener = this.deviceStateService.firstTouch.subscribe((e: TouchEvent) => {
-        $(this.bodySelector).addClass(this.hasTouchClass); 
+        $(this.bodySelector).addClass(this.hasTouchClass);
         touchListener.unsubscribe();
     });
   }
